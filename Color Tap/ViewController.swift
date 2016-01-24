adc42e8317e72cdc7a00bfc2c297c27f7a87d3dd
Praveen Gowda I V <praveen.vvstgy@gmail.com>
2016-01-24 21:04:10 +0530

    Initial Commit

---
+++ Color Tap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Color Tap
+//
+//  Created by Praveen Gowda I V on 1/24/16.
+//  Copyright © 2016 Praveen Gowda I V. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
